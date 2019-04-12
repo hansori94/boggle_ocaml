134b41f4b0b0d230349a32b0dd5382ddb6235941
Jared DeCesare <jtd97@cornell.edu>
2019-04-12 15:51:26 -0400

    documentation

--- parse.mli
+++ parse.mli
@@ -2,19 +2,38 @@
    This module is responsible for parsing player input
 *)
 
-(** TODO DOCUMENT *)
+(**The type [words] represents the word that can be part of a 
+    player input.  Each word of represents a word of the [words] input
+    , where a {i word} is defined as a consecutive sequence of non-space 
+    characters. *)
 type words = string
 
-(** TODO DOCUMENT *)
+(** Represents a player input. Decomposes the input to a verb and 
+    a [words].  *)
 type input = 
   | Quit
   | Word of words
 
-(** TODO DOCUMENT *)
+(** Raised if empty input is encountered. *)
 exception Empty
 
-(** TODO DOCUMENT *)
+(** Raised if malformed input is encountered. *)
 exception Malformed
 
-(** TODO DOCUMENT *)
+(** [parse str] parses a player's input into a [input], as follows. The first
+    word (i.e., consecutive sequence of non-space characters) of [str] becomes 
+    the verb. The next word if any, become the object phrase.
+    Examples: 
+    - [parse "    quit game   "] is [Quit "game"]
+    - [parse "Word word"] is [Word "word"]. 
+
+    Requires: [str] contains only alphanumeric (A-Z, a-z, 0-9) and space 
+    characters (only ASCII character code 32; not tabs or newlines, etc.).
+
+    Raises: [Empty] if [str] is the empty string or contains only spaces. 
+
+    Raises: [Malformed] if the input is malformed. An input
+    is {i malformed} if the verb is neither "quit" nor "word",
+    or if the verb is "quit" and there is an input besides "game",
+    or if the verb is "word" and there is an empty object phrase.*)
 val parse : string -> input
