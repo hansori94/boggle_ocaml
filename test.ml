a21fc2766f01654647dab24b24c39d3c4c913ca0
Hannah Lee <hl838@cornell.edu>
2019-04-12 15:32:11 -0400

    test file

--- test.ml
+++ test.ml
@@ -1,11 +1,16 @@
 open OUnit2
+open State
 
 
+let state_tests = [
+  "placeholder" >:: (fun _ -> assert_equal 1 1);
+]
 
-(* let tests =
-   "test suite for midterm"  >::: List.flatten [
-    index_tests;
 
-   ]
+let tests =
+  "test suite for midterm"  >::: List.flatten [
+    state_tests;
 
-   let _ = run_test_tt_main tests *)
+  ]
+
+let _ = run_test_tt_main tests
