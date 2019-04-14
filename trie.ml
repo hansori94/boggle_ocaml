cc8174f43b87df7ef75cab242ce758d1c53e73f8
Zara Schreiber <zjs28@cornell.edu>
2019-04-14 15:21:27 -0400

    functions for insert

--- trie.ml
+++ trie.ml
@@ -14,12 +14,9 @@ let is_empty trie =
   match trie with
   | Node(" ", [], false) -> true
   | _ -> false
-<<<<<<< HEAD
 
-let get_node_val node =
-  match node with 
-  | Node(c, _, _) -> c
-=======
+
+
 (* 
 let parse filename d =
   let fcode = Pervasives.open_in (d^Filename.dir_sep^filename) in 
@@ -37,7 +34,7 @@ let parse filename d =
 
 (** [words f] is a list of all words in a .txt file [f]*)
 let words f = 
-  let file = Pervasives.open_in "example.txt" in
+  let file = Pervasives.open_in f in
   let lst = [] in
   let return = 
     let rec getwords f lst = 
@@ -47,23 +44,55 @@ let words f =
     getwords (file) (lst) in
   Pervasives.close_in file;
   return 
->>>>>>> fcb851d02c8bbdce4773589d8aae0ba7b39ec834
 
+let rec contains_key key nodelist = 
+  match nodelist with
+  | Node(c, _, _)::t -> 
+    if key = c then true else contains_key key t
+  | [] -> false
 
+let rec get_key_node key nodelist = 
+  match nodelist with
+  | Node(c, l, b)::t ->
+    if key = c then Node(c, l, b) else get_key_node key t
+  | [] -> failwith "this key does not exist in this list"
 
-let insert_char k trie (islast:bool) = 
-  match trie with
-  | Node(" ", [], false) -> Node(" ", [Node(k, [], islast)], false)
-  | Node(c, [], b) -> Node(c, [Node(k, [], islast)], b)
-  | Node(c, list, b) ->
-    if List.mem Node(k, _, _) list then Node(c, list, b) else
+let check_bool_matching node bool=
+  match node with
+  |Node(_, _, b) -> if b = bool then true else false
 
-      (** TODO DOCUMENT BETTER
-          takes in a key and a trie and returns a new trie with the key added
-          or if the key is already there just returns the same trie
-      *)
-      let insert key trie : t = 
-        failwith "unimplemented"
+(** TODO DOCUMENT BETTER
+    remove the node with the given key from the nodelist
+*)
+let rec remove_key nodelist key acc=
+  match nodelist with
+  | Node(c, l, b)::t ->
+    if key = c then remove_key t key acc  else remove_key t key (Node(c,l,b)::acc)
+  | [] -> acc
+
+let update_key_bool nodelist key = 
+  match nodelist with
+  | Node(c, l, b)::t ->
+    if key = c then 
+
+      let insert_char k trie (islast:bool) = 
+        match trie with
+        | Node(" ", [], false) -> Node(" ", [Node(k, [], islast)], false)
+        | Node(c, [], b) -> Node(c, [Node(k, [], islast)], b)
+        | Node(c, list, b) ->
+          if contains_key k list then 
+            let curr = get_key_node key list in
+            if check_bool_matching curr islast then Node(c, list, b) else
+            if islast = false then Node(c, list, b) else
+              let new_list = Node(k, )::(remove_key list key [])
+                               Node(c, blah, b)
+
+(** TODO DOCUMENT BETTER
+    takes in a key and a trie and returns a new trie with the key added
+    or if the key is already there just returns the same trie
+*)
+let insert key trie : t = 
+  failwith "unimplemented"
 (* match trie with 
    | empty -> Node(" ", [], false) *)
 
