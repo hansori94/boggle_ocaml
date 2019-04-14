51942c37745fd9723835daf519e9a4b3af9b9a2a
Hannah Lee <dwingule@gmail.com>
2019-04-14 17:46:34 -0400

    insert

--- trie.ml
+++ trie.ml
@@ -4,6 +4,8 @@ type key = string
 (* the key is the value at the node (ex: "a") and the t list is its children
    and the bool is whether its the end of a word *)
 type t = Node of key * t list * bool
+(* type t = Node of key * t array * bool *)
+
 
 
 let empty : t = 
@@ -29,27 +31,90 @@ let words f =
   Pervasives.close_in file;
   return 
 
+(** [get_children t] returns the children of the trie [t] *)
+let get_children t = 
+  match t with
+  | Node (_, lst, _) -> lst
+
+(** [get_children lst acc] returns the list of keys of trie list [lst] 
+    Requires: [acc] is empty *)
+let rec get_keys lst acc : key list = 
+  match lst with 
+  | h::t -> begin 
+      match h with
+      | Node (k, _, _) -> 
+        get_keys t (k::acc)
+    end
+  | [] -> acc
+
+(** [find_subtrie lst c] is a trie [t] with a key value [c] from the list of
+    tries [lst] *)
+let rec find_subtrie lst c = 
+  match lst with
+  | h::t -> begin 
+      match h with
+      | Node (k, _, _) -> 
+        if k = c then h else find_subtrie t c
+    end
+  | [] -> failwith "c is not in lst"
+
+(** [update_children t lst] is a trie [t] with its children tries 
+    replaced with [lst]
+*)
+let update_children t lst = 
+  match t with
+  | Node (k, _, bool) -> Node (k, lst, bool)
+
+
+let rec remove_subtrie lst c acc = 
+  match lst with
+  | h::t -> if h<>c then remove_subtrie t c (c::acc) else remove_subtrie t c (acc)
+  | [] -> acc
+
+let make_true t = 
+  match t with
+  | Node(k, lst, _) -> Node (k, lst, true)
 
 (** [insert_word t lst] is a trie [t] with the word represented by [lst] 
     inserted. 
     Requires: 
     [lst] is a character list of the word ( "ace" -> ['a';'c';'e'] 
-
 *)
-(* let rec insert_word trie lst = 
-   match lst with
-   | h::t -> 
-    if h not in trie.[]:
-      insert h into trie.[]
-      if t=[]:
-        trie.[h].bool = true
-      insert_word trie.[h] t
-   else:
-      if t=[]:
-        trie.[h].bool = true
-      insert_word trie.[h] t
 
-   | [] -> trie *)
+
+let rec insert_word trie lst root = 
+  let children_list = get_children root in (* children tries of current root *)
+  let keys_list = get_keys children_list [] in (* keys of the children *)
+  match lst with (* matching each character of the word *)
+  | h::t -> 
+    (* if a given character is not a node in a trie yet *)
+    if (List.mem h (keys_list)) 
+    then 
+      insert_word trie t (find_subtrie children_list h)
+    else
+      let new_trie = Node (h, [], false) in
+      (* creating a new list of children with a new trie 
+         with value [h] appended *)
+      let updated_trie = update_children trie (new_trie::children_list) in
+      (* recursively calling the function with the new children, 
+         with [root] set to the next subtrie *)
+      insert_word updated_trie t (find_subtrie children_list h)
+  | [] -> trie 
+
+  (* | h::[] -> 
+     if (List.mem h (keys_list)) 
+     then 
+     let update_list = update_children (trie (make_true (find_subtrie children_list h)))
+
+     insert_word trie [] (find_subtrie children_list h)
+     else
+     let new_trie = Node (h, [], true) in
+     let updated_list = update_children trie (new_trie::children_list) in
+     insert_word updated_list [] (find_subtrie children_list h) *)
+  | [] -> trie 
+(* with last node changed to true *)
+
+
 
 
 
