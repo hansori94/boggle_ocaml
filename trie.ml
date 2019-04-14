db00e6d852533883823e6dfce91bd3c46a2d6b17
Zara Schreiber <zjs28@cornell.edu>
2019-04-14 16:10:08 -0400

    updated search

--- trie.ml
+++ trie.ml
@@ -30,7 +30,7 @@ let words f =
   return 
 
 
-(** [insert_word t lst] is a trie [t] with the word represented by [lst] 
+(* * [insert_word t lst] is a trie [t] with the word represented by [lst] 
     inserted. 
     Requires: 
     [lst] is a character list of the word ( "ace" -> ['a';'c';'e'] 
@@ -54,68 +54,55 @@ let words f =
 
 
 
-(* let rec contains_key key nodelist = 
-   match nodelist with
-   | Node(c, _, _)::t -> 
+let rec contains_key key nodelist = 
+  match nodelist with
+  | Node(c, _, _)::t -> 
     if key = c then true else contains_key key t
-   | [] -> false
+  | [] -> false
 
-   let rec get_key_node key nodelist = 
-   match nodelist with
-   | Node(c, l, b)::t ->
+let rec get_key_node key nodelist = 
+  match nodelist with
+  | Node(c, l, b)::t ->
     if key = c then Node(c, l, b) else get_key_node key t
-   | [] -> failwith "this key does not exist in this list"
+  | [] -> failwith "this key does not exist in this list"
 
-   let check_bool_matching node bool=
-   match node with
-   |Node(_, _, b) -> if b = bool then true else false
-
-   (** TODO DOCUMENT BETTER
-    remove the node with the given key from the nodelist
- *)
-   let rec remove_key nodelist key acc=
-   match nodelist with
-   | Node(c, l, b)::t ->
-    if key = c then remove_key t key acc  else remove_key t key (Node(c,l,b)::acc)
-   | [] -> acc
-
-<<<<<<< HEAD
-
-(* if contains_key k list then 
-   let curr = get_key_node key list in
-   if check_bool_matching curr islast then Node(c, list, b) else
-   if islast = false then Node(c, list, b) else
-    let new_list = Node(k, )::(remove_key list key [])
-                     Node(c, blah, b) *)
-=======
-   let update_key_bool nodelist key = 
-   match nodelist with
-   | Node(c, l, b)::t ->
-    if key = c then 
-
-      let insert_char k trie (islast:bool) = 
-        match trie with
-        | Node(" ", [], false) -> Node(" ", [Node(k, [], islast)], false)
-        | Node(c, [], b) -> Node(c, [Node(k, [], islast)], b)
-        | Node(c, list, b) ->
-          if contains_key k list then 
-            let curr = get_key_node key list in
-            if check_bool_matching curr islast then Node(c, list, b) else
-            if islast = false then Node(c, list, b) else
-              let new_list = Node(k, )::(remove_key list key [])
-                               Node(c, blah, b) *)
->>>>>>> 946ffcda94db6dd9cd45abf569014da974a046a2
+let check_bool_matching node bool=
+  match node with
+  |Node(_, _, b) -> if b = bool then true else false
 
 (** TODO DOCUMENT BETTER
-    takes in a key and a trie and returns a new trie with the key added
-    or if the key is already there just returns the same trie
+    remove the node with the given key from the nodelist
 *)
+let rec remove_key nodelist key acc=
+  match nodelist with
+  | Node(c, l, b)::t ->
+    if key = c then remove_key t key acc  else remove_key t key (Node(c,l,b)::acc)
+  | [] -> acc
+
+
+
+
 let insert key trie : t = 
   failwith "unimplemented"
 
 
-(** TODO DOCUMENT BETTER
-    returns whether a given key is present in a trie
-*)
-let search key trie : bool = 
-  failwith "Unimplemented"
+
+let search key trie: bool = 
+  let full_length = String.length key in 
+  let rec search_help key trie n= 
+    let length = String.length key in 
+    match trie, length with 
+    | Node(c, list, b), length -> 
+      if n = full_length - 1 then begin
+        match trie with 
+        | Node(c, list, b) -> 
+          if b = true then true else false
+      end else
+      if contains_key (Char.escaped key.[n]) list then
+        let curr = get_key_node (Char.escaped key.[n]) list in 
+        search_help (String.sub key (n+1) (length - 1)) curr (n+1) else false
+
+  in 
+
+  search_help key trie 0
+
